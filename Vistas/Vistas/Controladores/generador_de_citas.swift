26595d232de9c612e96ecb58eeb3f8d944bbff73
InkyMcWinky <121594803+InkyMcWinky@users.noreply.github.com>
2024-09-23 10:26:45 -0600

    Add files via upload

---
+++ Vistas/Vistas/Controladores/generador_de_citas.swift
@@ -0,0 +1,8 @@
+//
+//  generador_de_citas.swift
+//  Vistas
+//
+//  Created by alumno on 9/23/24.
+//
+
+import Foundation
---
+++ Vistas/Vistas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vistas
+//
+//  Created by alumno on 9/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBSegueAction func al_abrir_pantalla_citas(_ coder: NSCoder) -> ControladorPantallaCitas
+}
+
