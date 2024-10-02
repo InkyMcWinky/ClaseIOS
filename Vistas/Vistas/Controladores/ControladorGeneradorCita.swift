1ee6b14c130a5bc72812130249f6c8ed1d976ff9
InkyMcWinky <121594803+InkyMcWinky@users.noreply.github.com>
2024-10-02 10:51:34 -0600

    Add files via upload

---
+++ Vistas/Vistas/Controladores/ControladorGeneradorCita.swift
@@ -0,0 +1,32 @@
+//
+//  ControladorGeneradorCita.swift
+//  Vistas
+//
+//  Created by alumno on 9/27/24.
+//
+
+import Foundation
+import UIKit
+class ControladorPantallaAgregarCita: UIViewController{
+    var quien_lo_dice: String = ""
+    var que_dice: String = ""
+    
+    var cita_creada: Cita? = nil
+    @IBOutlet weak var quien_lo_dijo_view: UITextField!
+    
+    @IBOutlet weak var que_es_lo_que_dijo_view: UITextField!
+    
+    //Cambiar Labels por TextFiELDS
+    
+  
+    @IBAction func agregar_cita_nueva(_ sender: UIButton) {
+        cita_creada = Cita(quien_lo_dijo: quien_lo_dijo_view.text!,
+                           que_dijo:que_es_lo_que_dijo_view.text!)
+    }
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+
+    
+}
--- Vistas/Vistas/ViewController.swift
+++ Vistas/Vistas/ViewController.swift
@@ -8,14 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var cita_para_enviar: Cita = Cita(quien_lo_dijo: "Creeper", que_dijo:"Tsss")
+    var citas_disponibles: GeneradorDeCitas = GeneradorDeCitas()
+    var numero_aleatorio: Int = Int.random(in: 0...25)
+    
+    @IBOutlet weak var labelcito: UILabel!
+    
+    
 
     override func viewDidLoad() {
+        citas_disponibles.generar_citas_falsas()
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        actualizar_cantidad()
+    }
+    func actualizar_cantidad(){
+        labelcito.text = String(citas_disponibles.citas_creadas.count)
     }
     
     @IBSegueAction func al_abrir_pantalla_citas(_ coder: NSCoder) -> ControladorPantallaCitas? {
-        return ControladorPantallaCitas(muro_texto: "LaPrueba", de quien: "Creeper", coder:coder)
+        return ControladorPantallaCitas(cita_para_citar: citas_disponibles.obtener_cita_aleatoria(), coder:coder)
     }
+    
+    
+    @IBAction func volver_a_pantalla_inicio(segue : UIStoryboardSegue){
+        let pantalla_citas = segue.source as? ControladorPantallaCitas{
+            citas_disponibles.agregar_cita(pantalla_agregar_citas.cita_creada!)
+        }
+        
+        
+        actualizar_cantidad()
+    }
+    
+        
 }
 
