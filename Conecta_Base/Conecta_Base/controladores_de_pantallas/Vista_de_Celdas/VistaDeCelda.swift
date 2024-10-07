c4941b5a0cf2024622d6255d4880397acc794337
InkyMcWinky <121594803+InkyMcWinky@users.noreply.github.com>
2024-10-07 10:33:58 -0600

    Add files via upload

---
+++ Conecta_Base/Conecta_Base/controladores_de_pantallas/Vista_de_Celdas/VistaDeCelda.swift
@@ -0,0 +1,13 @@
+//
+//  VistaDeCeldaCollectionViewCell.swift
+//  Conecta_Base
+//
+//  Created by alumno on 10/7/24.
+//
+
+import UIKit
+
+class VistaDeCelda: UICollectionViewCell {
+    
+    @IBOutlet weak var etiqueta: UILabel!
+}
